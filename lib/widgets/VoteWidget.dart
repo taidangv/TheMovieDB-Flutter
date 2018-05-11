48c85ea74c9d41064171242182d4665ed91b53ee
Tai.DangVan <tai.dangvan@2359media.com>
2018-05-11 17:02:50 +0700

    enhance-vote-widget

--- lib/widgets/VoteWidget.dart
+++ lib/widgets/VoteWidget.dart
@@ -7,12 +7,31 @@ class VoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      vote.toString(),
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 24.0,
+    return new Container(
+      decoration: BoxDecoration(
+        color: Color(0x88000000),
+        borderRadius: BorderRadius.all(new Radius.circular(30.0)),
+      ),
+      padding: EdgeInsets.only(top: 1.0, bottom: 1.0, left: 7.0, right: 7.0),
+      margin: EdgeInsets.all(5.0),
+      child: new Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(Icons.star, color: _getVoteColor()),
+          Text(
+            vote.toString(),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Color _getVoteColor() {
+    return Color.lerp(Colors.red[900], Colors.yellowAccent, (vote - 5) / 5);
   }
 }
